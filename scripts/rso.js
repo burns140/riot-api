366b82a5856e559d19f9eb1e274d0bae57790df3
Stephen Burns <burns140@purdue.edu>
2021-11-14 20:53:22 -0700

    refactor AuthenticationHandler to static functions

--- scripts/rso.js
+++ scripts/rso.js
@@ -4,17 +4,14 @@ const ENTITLEMENTS_URL = "https://entitlements.auth.riotgames.com/api/token/v1";
 const USER_INFO_URL = "https://auth.riotgames.com/userinfo";
 const USERNAME = "aphantomdolphin";
 const PASSWORD = "c7eEF9!m$4demPcY";
+const axiosInstance = axios.create({ withCredentials: true });
+const headers = {};
+const config = {
+    headers: headers
+};
 
 class AuthenticationHandler {
-    axiosInstance = axios.create({ withCredentials: true });
-    headers = {};
-    config = {
-        headers: this.headers
-    };
-
-    constructor(){}
-
-    async createSession() {
+    static async createSession() {
         const initRequest = {
             "client_id": "play-valorant-web-prod",
             "nonce": "1",
@@ -24,17 +21,17 @@ class AuthenticationHandler {
     
         const response = await axios.post(AUTH_URL, initRequest);
         const cookie = response.headers["set-cookie"];
-        this.axiosInstance.defaults.headers.Cookie = cookie;
+        axiosInstance.defaults.headers.Cookie = cookie;
     }
 
-    async getAccessToken() {
+    static async getAccessToken() {
         let data = {
             "type": "auth",
             "username": USERNAME,
             "password": PASSWORD    
         }
         
-        const response = await this.axiosInstance.put(AUTH_URL, data);
+        const response = await axiosInstance.put(AUTH_URL, data);
         const pattern = /access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)/;
         const strToMatch = response.data.response.parameters.uri;
         const matchArr = strToMatch.match(pattern);
@@ -43,31 +40,31 @@ class AuthenticationHandler {
         return accessId;
     }
 
-    async getEntitlementsToken() {
-        const response = await this.axiosInstance.post(ENTITLEMENTS_URL, {}, this.config);
+    static async getEntitlementsToken() {
+        const response = await axiosInstance.post(ENTITLEMENTS_URL, {}, config);
         const entitlementsToken = response.data["entitlements_token"]
     
         return entitlementsToken;
     }
 
-    async getUserId() {
-        const response = await this.axiosInstance.post(USER_INFO_URL, {}, this.config);
+    static async getUserId() {
+        const response = await axiosInstance.post(USER_INFO_URL, {}, config);
         const userId = response.data.sub;
     
         return userId;
     }
 
-    async run() {
+    static async run() {
         await this.createSession();
 
         const accessToken = await this.getAccessToken();
-        this.headers["Authorization"] = `Bearer ${accessToken}`;
+        headers["Authorization"] = `Bearer ${accessToken}`;
     
         const entitlementsToken = await this.getEntitlementsToken(accessToken);
-        this.headers["X-Riot-Entitlements-JWT"] = entitlementsToken;
+        headers["X-Riot-Entitlements-JWT"] = entitlementsToken;
         
         const userId = await this.getUserId();
     }
 }
 
-new AuthenticationHandler().run();
+AuthenticationHandler.run();
