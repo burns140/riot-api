a6908c5234cae6f4e6aa480b9e94f9a41fc883ef
Stephen Burns <burns140@purdue.edu>
2021-11-17 17:52:05 -0700

    add doc comments to BiMap

--- src/classes/models/BiMap.js
+++ src/classes/models/BiMap.js
@@ -4,27 +4,53 @@ module.exports = class BiMap {
 
     constructor() {}
 
+    /**
+     * @description Add entry to the map and reverse map
+     * @param {string} key 
+     * @param {string} value 
+     */
     set(key, value) {
         this._map.set(key, value);
         this._reverseMap.set(value, key);
     }
 
+    /**
+     * @description Get value from the original map
+     * @param {string} key 
+     * @returns {string | object}
+     */
     get(key) {
         return this._map.get(key);
     }
 
+    /**
+     * @description Call forEach on the original map
+     * @param {Function} callback 
+     */
     forEach(callback) {
         this._map.forEach(callback);
     }
 
+    /**
+     * @description Get value from reverse map, effectively getting a key from a value
+     * @param {string | object} value 
+     * @returns {string | object}
+     */
     getFromValue(value) {
         return this._reverseMap.get(value);
     }
 
+    /**
+     * @description Return the keys of the original map
+     * @returns {object}
+     */
     keys() {
         return this._map.keys();
     }
 
+    /**
+     * Getters are below
+     */
     get Map() {
         return this._map;
     }
